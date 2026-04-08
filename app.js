e879dc4c424866953523f3f55a70922e678bacc2
atharvbind <atharvbind77@gmail.com>
2026-04-08 20:15:46 +0530

    added minds dataset from multiple  cateogories

--- app.js
+++ app.js
@@ -2,25 +2,51 @@ let searchBtn=document.querySelector('button')
 let cards=document.getElementById('cards')
 let searchHandle = async ()=>{
     try{
-        
-            let name = document.querySelector('input').value 
-            await getSummary(name)
-
+        let name = document.querySelector('input').value 
+        await getSummary(name)
     }
     catch(err) {
         cards.innerHTML="<p>Error</p>"
     }
 }
+
 let input=document.querySelector('input')
+let minds=[]
+
+
+async function getMinds() {
+    //indina
+    let res1 = await fetch('https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Indian mathematicians&cmlimit=100&format=json&origin=*')
+    let data1 = await res1.json()
+    let arr1 = data1.query.categorymembers
+        .filter(x => x.ns == 0 && !x.title.toLowerCase().includes('list'))
+        .map(x => x.title)
+    //american
+    let res2 = await fetch('https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:American mathematicians&cmlimit=100&format=json&origin=*')
+    let data2 = await res2.json()
+    let arr2 = data2.query.categorymembers
+        .filter(x => x.ns == 0 && !x.title.toLowerCase().includes('list'))
+        .map(x => x.title)
+    //british
+    let res3 = await fetch('https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:British mathematicians&cmlimit=100&format=json&origin=*')
+    let data3 = await res3.json()
+    let arr3 = data3.query.categorymembers
+        .filter(x => x.ns == 0 && !x.title.toLowerCase().includes('list'))
+        .map(x => x.title)
+    minds = [...new Set([...arr1, ...arr2, ...arr3])]
+    console.log(minds.length)
+    console.log(minds)
+}
+getMinds()
 input.addEventListener('keydown',(e)=>{
     if (e.key=='Enter') {
         searchBtn.click()
     }
 })
 searchBtn.addEventListener('click',searchHandle)
-async function getSummary(n) {
+async function getSummary(query) {
     cards.innerHTML="<h2>Loading</h2>"
-  const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(n)}`;
+  const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(query)}`;
   
   const res = await fetch(url);
   if (!res.ok) throw new Error("Not found");
@@ -40,5 +66,4 @@ async function getSummary(n) {
   card.appendChild(name)
   card.appendChild(desc)
   cards.appendChild(card)
-
 }
