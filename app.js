80c9a15cf170949fc6e30c512e3557b4493da3fb
atharvbind <atharvbind77@gmail.com>
2026-04-08 20:53:46 +0530

    added autocomplete with suggestions and fallback filtering

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ let searchHandle = async ()=>{
 }
 
 let input=document.querySelector('input')
+let suggestions=document.getElementById('suggestions')
 let minds=[]
 
 
@@ -34,16 +35,63 @@ async function getMinds() {
         .filter(x => x.ns == 0 && !x.title.toLowerCase().includes('list'))
         .map(x => x.title)
     minds = [...new Set([...arr1, ...arr2, ...arr3])]
+    cards.innerHTML=''
     console.log(minds.length)
     console.log(minds)
 }
-getMinds()
 input.addEventListener('keydown',(e)=>{
     if (e.key=='Enter') {
         searchBtn.click()
     }
 })
+input.addEventListener('input',handleSuggestions)
 searchBtn.addEventListener('click',searchHandle)
+function showSuggestions(ls) {
+    suggestions.innerHTML=''
+    ls.forEach((name)=>{
+        let suggestion=document.createElement('div')
+        suggestion.innerText=name
+        suggestion.addEventListener('click',(e)=>{
+            getSummary(e.target.innerText)
+            suggestions.innerHTML=''
+            input.value=''
+        })
+        suggestions.appendChild(suggestion)
+    })
+}
+async function handleSuggestions() {
+    let val = input.value.toLowerCase().trim()
+    if (!val) {return}
+    let matches=minds.filter((x)=>x.toLowerCase().startsWith(val))
+    if (matches.length>0) {
+        showSuggestions(matches.slice(0,5))
+    } else {
+        let res = await fetch(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${val}&limit=5&origin=*`);
+        let data=await res.json()
+        let results = data[1];
+        results = results.filter(name => {
+            let n = name.toLowerCase();
+            let words = name.split(" ");
+            return (
+                words.length >= 2 &&
+                !n.includes("theorem") &&
+                !n.includes("formula") &&
+                !n.includes("equation") &&
+                !n.includes("function") &&
+                !n.includes("identity") &&
+                !n.includes("constant") &&
+                !n.includes("list") &&
+                !n.includes("algorithm") &&
+                !n.includes("method") &&
+                !n.includes("law") &&
+                !n.includes("city") &&
+                !n.includes("district") &&
+                !n.includes("university")
+            ) })
+        showSuggestions(results.slice(0,5))
+    }
+
+}
 async function getSummary(query) {
     cards.innerHTML="<h2>Loading</h2>"
   const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(query)}`;
@@ -67,3 +115,7 @@ async function getSummary(query) {
   card.appendChild(desc)
   cards.appendChild(card)
 }
+window.onload = () => {
+    cards.innerHTML = "<h2>Loading mathematicians...</h2>";
+    getMinds();
+};
